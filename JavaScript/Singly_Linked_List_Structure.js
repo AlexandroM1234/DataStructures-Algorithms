64bf03a7cb76086308ffd84d17dcb0db2f74f112
Alex Martinez <alexmartinez0071234@gmail.com>
2020-10-24 11:38:47 -0400

    added in coments

--- JavaScript/Singly_Linked_List_Structure.js
+++ JavaScript/Singly_Linked_List_Structure.js
@@ -1,3 +1,4 @@
+// Define a node with a value and a next pointer
 class Node {
   constructor(val) {
     this.val = val;
@@ -6,20 +7,28 @@ class Node {
 }
 
 class SinglyLinkedList {
+  //Linked List has a head tail and length
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+  // Add a node to the linked list
   push(val) {
+    // create a new node with a given value
     var newNode = new Node(val);
+    // if the LL is empty it becomes the head and the tail
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
     } else {
+      // if the LL is not empty
+      // The tails next pointer points to  the new node
       this.tail.next = newNode;
+      // then the tail is the new node
       this.tail = newNode;
     }
+    // after everthing increment the length and return the LL
     this.length++;
     return this;
   }
--- Python/SinglyLinkedList.py
+++ Python/SinglyLinkedList.py
@@ -1,21 +1,30 @@
+# Define a node with a value and a next pointer
 class Node:
     def __init__(self,val):
         self.val = val
         self.next = None
 
 class SinglyLinkedList:
+    # Linked List has a head tail and length
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
+    # Add a node to the linked list
     def push(self,val):
+        # create a new node with a given value
         newNode = Node(val)
+        # if the LL is empty it becomes the head and the tail
         if not self.head:
             self.head = newNode
             self.tail = self.head
+        # if the LL is not empty
         else:
+            # The tails next pointer points to  the new node
             self.tail.next = newNode
+            # then the tail is the new node
             self.tail = newNode
+        # after everthing increment the length and return the LL
         self.length += 1
         return self
 

35661da665da0719796970946a5b708f7325356a
Alex Martinez <alexmartinez0071234@gmail.com>
2020-10-29 17:47:44 -0400

    BST instert implemented

---
+++ JavaScript/BinarySearchTree.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  // insert a node into the binary search tree
+  insert(val) {
+    // create the new node
+    const newNode = new Node(val);
+    // if there is no root the new node becomes the root
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    // if there are other nodes in the tree traverse the tree like a linked list
+    let current = this.root;
+    while (current) {
+      // if the value inputed is less than the current value traverse the left subtree
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        // if there is no left on the current node the new node becomes the current's left
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        // if there is a node  on the current's left that node becomes the new current and keeps traversing
+        current = current.left;
+      } // same process except the value is greater than the current's value and taverses the right subtree
+      else if (val > current.val) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree);

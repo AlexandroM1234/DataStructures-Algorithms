9cad10e75a27b12f49cf5f294cab8c1e5e105c3b
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-09 13:53:49 -0500

    Hashtable started and basic hash func made

---
+++ JavaScript/HashTable.js
@@ -0,0 +1,17 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  // basic hashing function
+  hash(key, arrayLen) {
+    let total = 0;
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % arrayLen;
+    }
+    return total;
+  }
+}

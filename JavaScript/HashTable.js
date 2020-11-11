d19c870cee9e7d5aa95efef0bd5e95175d7d0a16
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-11 00:00:26 -0500

    comments added in HT

--- JavaScript/HashTable.js
+++ JavaScript/HashTable.js
@@ -14,23 +14,33 @@ class HashTable {
     }
     return total;
   }
+  // set a key value pair in the Hash Table
   set(key, val) {
+    // set the index of the key by hashing the key
     let index = this.hash(key);
+    // if there is no index from the given key set it as a new array
     if (!this.keyMap[index]) {
       this.keyMap[index] = [];
     }
+    // in all cases at the index of the keyMap push the key value pair into a new sub-array
     this.keyMap[index].push([key, val]);
     return this;
   }
+  // get the values at a certain key
   get(key) {
+    // look up the index
     let index = this.hash(key);
+    // if the index is valid
     if (this.keyMap[index]) {
+      // loop through the array at that index
       for (let i = 0; i < this.keyMap[index].length; i++) {
+        // if the first value of the sub-array is the key return the key value pair
         if (this.keyMap[index][i][0] === key) {
           return this.keyMap[index][i];
         }
       }
     }
+    // if the index is invalid return undefined
     return undefined;
   }
 }

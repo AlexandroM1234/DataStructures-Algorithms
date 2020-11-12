458ed27d0a0553c71b5719e7ec39ec8f1d73aa3f
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-12 12:49:47 -0500

    remove edge made

--- JavaScript/Graphs.js
+++ JavaScript/Graphs.js
@@ -14,6 +14,14 @@ class Graph {
     this.adjacencyList[v2].push(v1);
     return this.adjacencyList;
   }
+  // remove an edge from one vertext to another
+  removeEdge(v1, v2) {
+    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
+    let i1 = this.adjacencyList[v1].indexOf(v2);
+    let i2 = this.adjacencyList[v2].indexOf(v1);
+    this.adjacencyList[v1].splice(i2);
+    this.adjacencyList[v2].splice(i1);
+  }
 }
 
 let g = new Graph();
@@ -21,5 +29,9 @@ let g = new Graph();
 g.addVertex("Tokyo");
 g.addVertex("Hong Kong");
 g.addVertex("Kyoto");
+g.addVertex("Dallas");
+g.addVertex("Austin");
+g.addVertex("DC");
 g.addEdge("Tokyo", "Kyoto");
-g.addEdge("Hong Kong", "Tokyo");
+g.addEdge("DC", "Austin");
+g.addEdge("Dallas", "Austin");

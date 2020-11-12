c50f1e6b4c9c27def54fd7ca961ad4a5b57f901a
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-12 13:13:20 -0500

    remove edge method fixed and removeVertex made

--- JavaScript/Graphs.js
+++ JavaScript/Graphs.js
@@ -2,7 +2,7 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
-  // add a new vertex to the adjancenct list
+  // add a new vertex to the adjancency list
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = new Array();
     return this;
@@ -16,11 +16,17 @@ class Graph {
   }
   // remove an edge from one vertext to another
   removeEdge(v1, v2) {
-    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
-    let i1 = this.adjacencyList[v1].indexOf(v2);
-    let i2 = this.adjacencyList[v2].indexOf(v1);
-    this.adjacencyList[v1].splice(i2);
-    this.adjacencyList[v2].splice(i1);
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  // remove a vertex from the graph and all of its connections
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
   }
 }
 
@@ -33,5 +39,10 @@ g.addVertex("Dallas");
 g.addVertex("Austin");
 g.addVertex("DC");
 g.addEdge("Tokyo", "Kyoto");
+g.addEdge("Hong Kong", "Kyoto");
 g.addEdge("DC", "Austin");
 g.addEdge("Dallas", "Austin");
+console.log(g.adjacencyList);
+g.removeVertex("Tokyo");
+g.removeVertex("DC");
+console.log(g.adjacencyList);

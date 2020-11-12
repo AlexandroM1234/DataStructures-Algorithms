683e4233e68214eb7b27ec7dcecdf150a2600ba9
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-12 12:30:17 -0500

    add edge method made with error handling

--- JavaScript/Graphs.js
+++ JavaScript/Graphs.js
@@ -3,14 +3,23 @@ class Graph {
     this.adjacencyList = {};
   }
   // add a new vertex to the adjancenct list
-  addVertex(key) {
-    this.adjacencyList[key] = new Array();
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = new Array();
     return this;
+  }
+  // add a relationship from one vertext to another
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+    return this.adjacencyList;
   }
 }
 
 let g = new Graph();
 
 g.addVertex("Tokyo");
-
-console.log(g.adjacencyList);
+g.addVertex("Hong Kong");
+g.addVertex("Kyoto");
+g.addEdge("Tokyo", "Kyoto");
+g.addEdge("Hong Kong", "Tokyo");

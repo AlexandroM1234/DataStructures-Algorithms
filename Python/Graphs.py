b5f566765af6498069b54d72525abccaefe4cd2b
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-18 13:44:40 -0500

    installed python formatter

--- Python/Graphs.py
+++ Python/Graphs.py
@@ -2,37 +2,38 @@ class Graph:
     """
     Graph made with an adjacenyList
     """
+
     def __init__(self):
         self.adjacencyList = {}
-    
+
     def addVertex(self, vertex):
         """
         add a vertex to the adjacency list
         """
         self.adjacencyList[vertex] = []
-    
-    def addEdge(self, v1,v2):
+
+    def addEdge(self, v1, v2):
         """
         add a connection from one vertex to another
         """
         self.adjacencyList[v1].append(v2)
         self.adjacencyList[v2].append(v1)
 
-    def removeEdge(self, v1 , v2):
+    def removeEdge(self, v1, v2):
         """
-        remove a connection from 2 nodes 
+        remove a connection from 2 nodes
         """
-        self.adjacencyList[v1] = list(filter(lambda x: x != v2,self.adjacencyList[v1]))
-        self.adjacencyList[v2] = list(filter(lambda x: x != v1,self.adjacencyList[v2]))
+        self.adjacencyList[v1] = list(filter(lambda x: x != v2, self.adjacencyList[v1]))
+        self.adjacencyList[v2] = list(filter(lambda x: x != v1, self.adjacencyList[v2]))
 
-    def removeVertex(self,vertex):
+    def removeVertex(self, vertex):
         """
         removes a vertex from the adjacency list
         """
 
         while len(self.adjacencyList[vertex]):
             adjacencyVertex = self.adjacencyList[vertex].pop()
-            self.removeEdge(vertex,adjacencyVertex)
+            self.removeEdge(vertex, adjacencyVertex)
 
         del self.adjacencyList[vertex]
 
@@ -46,7 +47,7 @@ class Graph:
 
         s.append(start)
 
-        while (len(s)):
+        while len(s):
             vertex = s.pop()
 
             visited[vertex] = True
@@ -66,10 +67,9 @@ class Graph:
         visited = {}
         output = []
         q = []
-
         q.append(start)
 
-        while (len(q)):
+        while len(q):
             vertex = q.pop(0)
 
             visited[vertex] = True
@@ -81,6 +81,8 @@ class Graph:
                     q.append(neighbor)
 
         return output
+
+
 g = Graph()
 
 g.addVertex("A")
@@ -90,13 +92,13 @@ g.addVertex("D")
 g.addVertex("E")
 g.addVertex("F")
 
-g.addEdge("A","B")
-g.addEdge("A","C")
-g.addEdge("B","D")
-g.addEdge("C","E")
-g.addEdge("D","E")
-g.addEdge("D","F")
-g.addEdge("E","F")
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
 print(g.adjacencyList)
 print(g.DFSI("A"))
 print(g.BFSI("A"))

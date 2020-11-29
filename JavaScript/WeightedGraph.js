f2bd1396287cc6a23489ec86da92cb27d6017919
Alex Martinez <alexmartinez0071234@gmail.com>
2020-11-29 13:15:12 -0500

    dijkstras Algorithm works in JS

--- JavaScript/WeightedGraph.js
+++ JavaScript/WeightedGraph.js
@@ -1,3 +1,18 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -10,14 +25,65 @@ class WeightedGraph {
     this.adjacencyList[v1].push({ node: v2, weight });
     this.adjacencyList[v2].push({ node: v1, weight });
   }
-}
 
+  dijkstrasAlgo(startV, endV) {
+    const distances = {};
+    const q = new PriorityQueue();
+    const prev = {};
+    const path = [];
+    let smallest;
+    for (const vertices in this.adjacencyList) {
+      if (vertices === startV) {
+        distances[vertices] = 0;
+        q.enqueue(vertices, 0);
+      } else {
+        distances[vertices] = Infinity;
+        q.enqueue(vertices, Infinity);
+      }
+      prev[vertices] = null;
+    }
+    while (q.values.length) {
+      smallest = q.dequeue().val;
+      if (smallest === endV) {
+        while (prev[smallest]) {
+          path.push(smallest);
+          smallest = prev[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (const neighbor in this.adjacencyList[smallest]) {
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          let candidate = distances[smallest] + nextNode.weight;
+          const nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            // updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            // updating previous - How we got to neighbor
+            prev[nextNeighbor] = smallest;
+            // enqueue in priority queue with new priority
+            q.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    return path.concat(smallest).reverse();
+  }
+}
 const g = new WeightedGraph();
 
 g.addVertex("A");
 g.addVertex("B");
 g.addVertex("C");
-g.addEdge("A", "B", 9);
-g.addEdge("A", "C", 5);
-g.addEdge("B", "C", 7);
-console.log(g.adjacencyList);
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B", 4);
+g.addEdge("A", "C", 2);
+g.addEdge("B", "E", 3);
+g.addEdge("C", "D", 2);
+g.addEdge("C", "F", 4);
+g.addEdge("D", "F", 1);
+g.addEdge("D", "E", 3);
+g.addEdge("F", "E", 1);
+console.log(g.dijkstrasAlgo("A", "F"));

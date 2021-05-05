15994f75bee18e2ef8f544b8ace5bdec4749675f
Alex Martinez <alexmartinez0071234@gmail.com>
2021-05-05 13:06:10 -0400

    binary search made with comments and doc string

--- JavaScript/SearchAlgorithms.js
+++ JavaScript/SearchAlgorithms.js
@@ -14,4 +14,30 @@ function linearSearch(arr, val) {
   return -1;
 }
 
+/**
+ * Binary Search NEEDS all the data in the array to be SORTED
+ * Given an array find a value in it
+ * with binary search go in the middle and see if the value is less or more than the value
+ * regardless of whether or not its greater or less than split the array by half every time untill the value is in the middle
+ * and best case its in the middle
+ * @param {Array} arr
+ * @param {*} val
+ * @param {number}
+ */
+function binarySearch(arr, val) {
+  let left = 0;
+  let right = arr.length - 1;
+  let half = Math.floor((left + right) / 2);
+  while (arr[half] !== val) {
+    if (val < arr[half]) {
+      right = half - 1;
+    } else {
+      left = half + 1;
+    }
+    half = Math.floor((left + right) / 2);
+  }
+  return arr[half] === val ? half : -1;
+}
+
 console.log(linearSearch([1, 2, 3, 4, 5, 10, 56, 78], 56));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));

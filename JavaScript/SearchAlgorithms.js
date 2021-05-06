de3a569cb5e03d8acf4ce4a58a3a329053c2cbe0
Alex Martinez <alexmartinez0071234@gmail.com>
2021-05-06 12:26:15 -0400

    naive string search algorithm

--- JavaScript/SearchAlgorithms.js
+++ JavaScript/SearchAlgorithms.js
@@ -2,9 +2,11 @@
  * Searches through an Array and looks for a value inside of it
  * If it's found the index of the value is returned
  * Otherwise it returns a -1
+ * Big O : O(N)
  * @param {Array} arr
  * @param {*} val
  * @return {number}
+ * @returns {number}
  */
 
 function linearSearch(arr, val) {
@@ -20,9 +22,11 @@ function linearSearch(arr, val) {
  * with binary search go in the middle and see if the value is less or more than the value
  * regardless of whether or not its greater or less than split the array by half every time untill the value is in the middle
  * and best case its in the middle
+ * Big O: O(log n)
  * @param {Array} arr
  * @param {*} val
  * @param {number}
+ * @returns {number}
  */
 function binarySearch(arr, val) {
   let left = 0;
@@ -39,5 +43,28 @@ function binarySearch(arr, val) {
   return arr[half] === val ? half : -1;
 }
 
+/**
+ * Given one string find if a given substring is in the string and return how many times is occurs
+ * This is a naive solution with O(n^2) time complexity
+ * @param {String} str
+ * @param {String} sub
+ * @returns {number}
+ */
+function stringSearch(str, sub) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < sub.length; j++) {
+      if (sub[j] !== str[i + j]) {
+        break;
+      }
+      if (j === sub.length - 1) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+
 console.log(linearSearch([1, 2, 3, 4, 5, 10, 56, 78], 56));
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));
+console.log(stringSearch("Larry ar ar ", "ar"));
